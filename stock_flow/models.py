11668e65381bcfcd93588f8bc146112d99a8453a
PiotrZettt <piotrzielinski@o2.pl>
2023-07-10 21:23:31 +0100

    Update data models for stock flow module

--- stock_flow/models.py
+++ stock_flow/models.py
@@ -12,7 +12,7 @@ class Project(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="projects")
     project_code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    picture = models.ImageField(upload_to='media/', blank=True)
+    picture = models.ImageField(upload_to="media/", blank=True)
 
     def __str__(self):
         return self.name
@@ -21,6 +21,9 @@ class Project(models.Model):
 class Part(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="parts")
     serial_number = models.CharField(max_length=200)
+    stage = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    dispatch_date = models.DateField(blank=True)
 
     def __str__(self):
         return self.serial_number
