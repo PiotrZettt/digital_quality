062f585df93ed8c53444beefc95bfd33312cd66a
PiotrZettt <piotrzielinski@o2.pl>
2023-07-11 10:07:43 +0100

    Update stock flow models

--- stock_flow/models.py
+++ stock_flow/models.py
@@ -10,7 +10,8 @@ class Customer(models.Model):
 
 class Project(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="projects")
-    project_code = models.CharField(max_length=20)
+    project_internal_code = models.CharField(max_length=20, blank=True)
+    project_customer_code = models.CharField(max_length=20, blank=True)
     name = models.CharField(max_length=200)
     picture = models.ImageField(upload_to="media/", blank=True)
 
@@ -18,11 +19,33 @@ class Project(models.Model):
         return self.name
 
 
+class CycleStage(models.Model):
+    cycle_stage_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.cycle_stage_name
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location_name
+
+
+class PartStatus(models.Model):
+    status_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.status_name
+
+
 class Part(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="parts")
     serial_number = models.CharField(max_length=200)
-    stage = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
+    stage = models.ForeignKey(CycleStage, on_delete=models.CASCADE, related_name="parts")
+    location = models.CharField(Location, on_delete=models.CASCADE, related_name="parts")
+    status = models.ForeignKey(PartStatus, on_delete=models.CASCADE, related_name="parts")
     dispatch_date = models.DateField(blank=True)
 
     def __str__(self):
